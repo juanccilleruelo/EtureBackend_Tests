e8987e7e2079c0bf2519878a56262daeed51ee6f
Juan C.Cilleruelo <10785381+juanccilleruelo@users.noreply.github.com>
2025-10-24 16:18:40 +0200

    Add unit tests for assays controller endpoints

--- EtureBackendTests.dpr
+++ EtureBackendTests.dpr
@@ -1,28 +1,29 @@
 program EtureBackendTests;
 
 uses
-  Vcl.Forms,
-  WEBLib.Forms,
-  TestBackendConnection in 'TestBackendConnection.pas',
-  TestAppIssues in 'TestAppIssues.pas',
-  TestCampaigns in 'TestCampaigns.pas',
-  TestPositions in 'TestPositions.pas',
-  TestParents in 'TestParents.pas',
-  TestInjuryAuxModels in 'TestInjuryAuxModels.pas',
-  TestAcademicRecords in 'TestAcademicRecords.pas',
-  senCille.WebSetup in '..\EtureFrontend\framework\senCille.WebSetup.pas',
-  senCille.Bootstrap in '..\EtureFrontend\framework\senCille.Bootstrap.pas',
-  senCille.MVCRequests in '..\EtureFrontend\framework\senCille.MVCRequests.pas',
-  ConfigurationConsts in '..\EtureFrontend\framework\ConfigurationConsts.pas',
-  senCille.Miscellaneous in '..\EtureFrontend\framework\senCille.Miscellaneous.pas',
-  Dummy in '..\EtureFrontend\Dummy\Dummy.pas' {DummyForm: TWebForm} {*.html},
-  senCille.CustomWebForm in '..\EtureFrontend\framework\senCille.CustomWebForm.pas' {scCustomWebForm: TWebForm} {*.html},
-  senCille.DataManagement in '..\EtureFrontend\framework\senCille.DataManagement.pas';
+   Vcl.Forms,
+   WEBLib.Forms,
+   TestBackendConnection in 'TestBackendConnection.pas',
+   TestAppIssues in 'TestAppIssues.pas',
+   TestCampaigns in 'TestCampaigns.pas',
+   TestAssays in 'TestAssays.pas',
+   TestPositions in 'TestPositions.pas',
+   TestParents in 'TestParents.pas',
+   TestInjuryAuxModels in 'TestInjuryAuxModels.pas',
+   TestAcademicRecords in 'TestAcademicRecords.pas',
+   senCille.WebSetup in '..\EtureFrontend\framework\senCille.WebSetup.pas',
+   senCille.Bootstrap in '..\EtureFrontend\framework\senCille.Bootstrap.pas',
+   senCille.MVCRequests in '..\EtureFrontend\framework\senCille.MVCRequests.pas',
+   ConfigurationConsts in '..\EtureFrontend\framework\ConfigurationConsts.pas',
+   senCille.Miscellaneous in '..\EtureFrontend\framework\senCille.Miscellaneous.pas',
+   Dummy in '..\EtureFrontend\Dummy\Dummy.pas' {DummyForm: TWebForm} {*.html},
+   senCille.CustomWebForm in '..\EtureFrontend\framework\senCille.CustomWebForm.pas' {scCustomWebForm: TWebForm} {*.html},
+   senCille.DataManagement in '..\EtureFrontend\framework\senCille.DataManagement.pas';
 
 {$R *.res}
 
 begin
-  Application.Initialize;
-  Application.MainFormOnTaskbar := True;
-  Application.Run;
+   Application.Initialize;
+   Application.MainFormOnTaskbar := True;
+   Application.Run;
 end.
